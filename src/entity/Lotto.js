e3c2c8ca1a735ece1acf1d0345e47a728c18a72e
Tnalxmsk <alstnwkd990@naver.com>
2025-11-02 02:17:37 +0900

    feat(Lotto): 로또 검증 기능 구현
    - 중복 숫자 검증 기능 구현
    - 로또 숫자 범위 검증 기능 구현

---
+++ src/entity/Lotto.js
@@ -0,0 +1,32 @@
+import { LOTTO_ERROR_MESSAGE, LOTTO_ERROR_TYPES } from "./const/error.js";
+import { LOTTO_COUNT, LOTTO_MAX_NUMBER, LOTTO_MIN_NUMBER, LOTTO_NUMBER_LENGTH } from "./const/lotto.const.js";
+import { throwError } from "../utils/error.js";
+
+class Lotto {
+  #numbers;
+
+  constructor(numbers) {
+    this.#validate(numbers);
+    this.#numbers = numbers;
+  }
+
+  get numbers() {
+    return this.#numbers;
+  }
+
+  #validate(numbers) {
+    if (numbers.length !== LOTTO_COUNT) {
+      throwError(LOTTO_ERROR_MESSAGE, LOTTO_ERROR_TYPES.MAX_COUNT);
+    }
+
+    if (numbers.some((number) => number < LOTTO_MIN_NUMBER || number > LOTTO_MAX_NUMBER)) {
+      throwError(LOTTO_ERROR_MESSAGE, LOTTO_ERROR_TYPES.INVALID_NUMBER);
+    }
+
+    if (new Set(numbers).size !== LOTTO_NUMBER_LENGTH) {
+      throwError(LOTTO_ERROR_MESSAGE, LOTTO_ERROR_TYPES.DUPLICATE);
+    }
+  }
+}
+
+export default Lotto;
