1fd93b3684ba0dad5219fed32bea0e524505b785
Tnalxmsk <alstnwkd990@naver.com>
2025-11-02 02:21:17 +0900

    feat(error.const): 에러 관련 상수화
    에러 타입, 에러 메시지 추가

---
+++ src/entity/const/error.const.js
@@ -0,0 +1,11 @@
+export const LOTTO_ERROR_TYPES = {
+  MAX_COUNT: "MAX_COUNT",
+  INVALID_NUMBER: "INVALID_NUMBER",
+  DUPLICATE: "DUPLICATE",
+};
+
+export const LOTTO_ERROR_MESSAGE = {
+  [LOTTO_ERROR_TYPES.MAX_COUNT]: "[ERROR] 로또 번호는 6개여야 합니다.",
+  [LOTTO_ERROR_TYPES.INVALID_NUMBER]: "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.",
+  [LOTTO_ERROR_TYPES.DUPLICATE]: "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.",
+};
