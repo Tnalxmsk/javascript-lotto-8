b4d57cb2512657c362baeca0c49f9dc9aea95aa7
Tnalxmsk <alstnwkd990@naver.com>
2025-11-02 16:06:19 +0900

    test(WinningLottoTest): 당첨 로또 기능 테스트
    보너스 번호를 가지고 있는지 확인하는 기능 테스트 코드 작성

--- __tests__/WinningLottoTest.js
+++ __tests__/WinningLottoTest.js
@@ -7,4 +7,16 @@ describe("당첨 로또 클래스 테스트", () => {
     const winningLotto = new WinningLotto([1, 2, 3, 4, 5, 8]);
     expect(winningLotto.countMatches(myLotto)).toBe(5);
   });
+
+  test("보너스 넘버를 가지고 있지 않으면 false를 반환한다.", () => {
+    const myLotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const winningLotto = new WinningLotto([1, 2, 3, 4, 5, 8], 10);
+    expect(winningLotto.hasBonusNumber(myLotto)).toBe(false);
+  });
+
+  test("보너스 넘버를 가지고 있다면 true를 반환한다.", () => {
+    const myLotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const winningLotto = new WinningLotto([1, 2, 3, 4, 5, 8], 1);
+    expect(winningLotto.hasBonusNumber(myLotto)).toBe(true);
+  })
 });
