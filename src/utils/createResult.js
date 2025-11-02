db0dec52486051700b4ff29112c27ce08630ff7a
Tnalxmsk <alstnwkd990@naver.com>
2025-11-03 02:58:51 +0900

    refactor(): 초기값 상수화

--- src/utils/createResult.js
+++ src/utils/createResult.js
@@ -1,8 +1,9 @@
-import { RANK } from "../service/const/rank.js";
+import { RANK } from "../service/const/rank.const.js";
+import { INITIAL_COUNT } from "../common/const.js";
 
 /**
  *  @returns {Array<{ rank: string, count: number }>} rank: RANK.FIRST | RANK.SECOND | RANK.THIRD | RANK.FOURTH | RANK.FIFTH | RANK.NONE
  */
 export const createResult = () => {
-  return Object.values(RANK).map((rank) => ({ rank, count: 0 }));
+  return Object.values(RANK).map((rank) => ({ rank, count: INITIAL_COUNT }));
 };
