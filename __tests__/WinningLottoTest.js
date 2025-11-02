f9576ab754762db15428386aa0bf7c207237eb84
Tnalxmsk <alstnwkd990@naver.com>
2025-11-02 18:02:43 +0900

    fix(WinningLottoTest): 테스트 실패 수정
    보너스 번호 생성자 전달을 통해 실패 수정

--- __tests__/WinningLottoTest.js
+++ __tests__/WinningLottoTest.js
@@ -4,7 +4,7 @@ import Lotto from "../src/entity/Lotto.js";
 describe("당첨 로또 클래스 테스트", () => {
   test("내 로또 번화 당첨 로또의 매칭 개수를 반환한다.", () => {
     const myLotto = new Lotto([1, 2, 3, 4, 5, 6]);
-    const winningLotto = new WinningLotto([1, 2, 3, 4, 5, 8]);
+    const winningLotto = new WinningLotto([1, 2, 3, 4, 5, 8], 10);
     expect(winningLotto.countMatches(myLotto)).toBe(5);
   });
 
@@ -15,8 +15,8 @@ describe("당첨 로또 클래스 테스트", () => {
   });
 
   test("보너스 넘버를 가지고 있다면 true를 반환한다.", () => {
-    const myLotto = new Lotto([1, 2, 3, 4, 5, 6]);
-    const winningLotto = new WinningLotto([1, 2, 3, 4, 5, 8], 1);
+    const myLotto = new Lotto([1, 2, 3, 4, 5, 10]);
+    const winningLotto = new WinningLotto([1, 2, 3, 4, 5, 8], 10);
     expect(winningLotto.hasBonusNumber(myLotto)).toBe(true);
   });
 
