ab1f2e304969037e92574a43ae6c4031486b41ec
Tnalxmsk <alstnwkd990@naver.com>
2025-11-02 17:26:24 +0900

    feat(lotto.const): 금액 관련 상수화

--- src/entity/const/lotto.const.js
+++ src/entity/const/lotto.const.js
@@ -1,3 +1,8 @@
 export const LOTTO_MIN_NUMBER = 1;
 export const LOTTO_MAX_NUMBER = 45;
 export const LOTTO_COUNT = 6;
+
+export const LOTTO_PRICE = 1000;
+export const MIN_LOTTO_AMOUNT = 1000;
+export const MAX_LOTTO_AMOUNT = 1000000;
+export const LOTTO_PRICE_REMAINDER = 0;
