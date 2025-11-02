21ccb5f8c4e591e6fcf64983b8e84b42bc431f8f
Tnalxmsk <alstnwkd990@naver.com>
2025-11-02 18:03:13 +0900

    style(TicketTest): describe 문자 수정
    로또 -> 티켓

--- __tests__/TicketTest.js
+++ __tests__/TicketTest.js
@@ -1,7 +1,7 @@
 import Lotto from "../src/entity/Lotto.js";
 import Ticket from "../src/entity/Ticket.js";
 
-describe("로또 클래스 테스트", () => {
+describe("티켓 클래스 테스트", () => {
   test("구매한 로또들의 총 금액을 반환한다.", () => {
     const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
     const lotto2 = new Lotto([1, 2, 3, 4, 5, 6]);
