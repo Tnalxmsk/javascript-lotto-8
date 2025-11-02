5a6d26018cf71b8557f3d6f3306ea0c8dca21f8e
Tnalxmsk <alstnwkd990@naver.com>
2025-11-03 02:31:23 +0900

    refactor(WinningLotto): 당첨 로또 개선
    검증 함수 이름 변경 및 보너스 번호 등록 순서 변경

--- __tests__/service/LottoResultCalculatorTest.js
+++ __tests__/service/LottoResultCalculatorTest.js
@@ -7,7 +7,8 @@ import { RANK } from "../../src/service/const/rank.js";
 describe("로또 결과 계산 클래스 테스트", () => {
   test("로또 번호가 5개가 일치하지만 보너스 번호가 존재하지 않아 THIRD 를 반환한다.", () => {
     const lottoResultCalculator = new LottoResultCalculator();
-    const winningLotto = new WinningLotto([1, 2, 3, 4, 5, 8], 10);
+    const winningLotto = new WinningLotto([1, 2, 3, 4, 5, 8]);
+    winningLotto.registerBonusNumber(10);
     const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
     const count = winningLotto.countMatches(lotto);
     const hasBonusNumber = winningLotto.hasBonusNumber(lotto);
@@ -17,7 +18,8 @@ describe("로또 결과 계산 클래스 테스트", () => {
 
   test("로또 번호가 5개가 일치하고 보너스 번호가 존재하면 SECOND 를 반환한다.", () => {
     const lottoResultCalculator = new LottoResultCalculator();
-    const winningLotto = new WinningLotto([1, 2, 3, 4, 5, 8], 10);
+    const winningLotto = new WinningLotto([1, 2, 3, 4, 5, 8]);
+    winningLotto.registerBonusNumber(10);
     const lotto = new Lotto([1, 2, 3, 4, 5, 10]);
     const count = winningLotto.countMatches(lotto);
     const hasBonusNumber = winningLotto.hasBonusNumber(lotto);
@@ -27,7 +29,8 @@ describe("로또 결과 계산 클래스 테스트", () => {
 
   test("로또 번호가 모두 일치하면 FIRST를 반화한다.", () => {
     const lottoResultCalculator = new LottoResultCalculator();
-    const winningLotto = new WinningLotto([1, 2, 3, 4, 5, 6], 10);
+    const winningLotto = new WinningLotto([1, 2, 3, 4, 5, 6]);
+    winningLotto.registerBonusNumber(10);
     const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
     const count = winningLotto.countMatches(lotto);
     const hasBonusNumber = winningLotto.hasBonusNumber(lotto);
@@ -37,7 +40,8 @@ describe("로또 결과 계산 클래스 테스트", () => {
 
   test("로또 번호 일치 개수가 3개 미만이면 NONE을 반환한다.", () => {
     const lottoResultCalculator = new LottoResultCalculator();
-    const winningLotto = new WinningLotto([1, 2, 3, 4, 5, 6], 10);
+    const winningLotto = new WinningLotto([1, 2, 3, 4, 5, 6]);
+    winningLotto.registerBonusNumber(10);
     const lotto = new Lotto([12, 13, 14, 15, 16, 17]);
     const count = winningLotto.countMatches(lotto);
     const hasBonusNumber = winningLotto.hasBonusNumber(lotto);
@@ -47,7 +51,8 @@ describe("로또 결과 계산 클래스 테스트", () => {
 
   test("1등에 당첨 되면 개수를 해당 RANK의 개수를 증가시킨다.", () => {
     const lottoResultCalculator = new LottoResultCalculator();
-    const winningLotto = new WinningLotto([1, 2, 3, 4, 5, 6], 10);
+    const winningLotto = new WinningLotto([1, 2, 3, 4, 5, 6]);
+    winningLotto.registerBonusNumber(10);
     const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
     const lotto2 = new Lotto([1, 2, 3, 4, 15, 36]);
     const ticket = new Ticket([lotto, lotto2]);
