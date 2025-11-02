341aae6836948f1972d007c1c7ed0638d3d98406
Tnalxmsk <alstnwkd990@naver.com>
2025-11-02 18:05:07 +0900

    test(LottoMachineTest): 로또 발행 기능 테스트
    로또 발행 기능 테스트 코드 작성

---
+++ __tests__/LottoMachineTest.js
@@ -0,0 +1,18 @@
+import Order from "../src/entity/Order.js";
+import LottoMachine from "../src/service/LottoMachine.service.js";
+
+describe("로또 발매기 클래스 테스트", () => {
+  test("로또 발행 테스트 - 주문 금액 만큼 로또를 발행한다.", () => {
+    const order = new Order(3000);
+    const lottoMachine = new LottoMachine();
+    const ticket = lottoMachine.issue(order);
+    expect(ticket.getCount()).toBe(order.count);
+  });
+
+  test("발행 테스트 - 주문 금액과 로또 발행 티켓의 총 금액과 일치한다.", () => {
+    const order = new Order(3000);
+    const lottoMachine = new LottoMachine();
+    const ticket = lottoMachine.issue(order);
+    expect(ticket.totalPrice()).toBe(order.amount);
+  });
+});
