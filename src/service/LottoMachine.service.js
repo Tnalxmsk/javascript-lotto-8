6b0ad0a150cf171b5a43b3d4fc3730902f6e42f7
Tnalxmsk <alstnwkd990@naver.com>
2025-11-02 18:04:36 +0900

    feat(LottoMachine): 로또 발행기 클래스 구현
    로또 발행 기능 구현

---
+++ src/service/LottoMachine.service.js
@@ -0,0 +1,28 @@
+import { Random } from "@woowacourse/mission-utils";
+import Lotto from "../entity/Lotto.js";
+import { LOTTO_COUNT, LOTTO_MAX_NUMBER, LOTTO_MIN_NUMBER } from "../common/const.js";
+import Ticket from "../entity/Ticket.js";
+
+class LottoMachine {
+
+  /**
+   * @param {Order} order
+   */
+  issue(order) {
+    const lottos = [];
+
+    for (let i = 0; i < order.count; i++) {
+      const lotto = this.#makeLotto();
+      lottos.push(lotto);
+    }
+
+    return new Ticket(lottos);
+  }
+
+  #makeLotto() {
+    const numbers = Random.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, LOTTO_COUNT).sort((a, b) => a - b);
+    return new Lotto(numbers);
+  }
+}
+
+export default LottoMachine;
